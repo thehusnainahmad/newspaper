c80103c4b8b0fbef5bd1a9d3b82bc10d3346e577
Husnain Ahmad <42809521+thehusnainahmad@users.noreply.github.com>
2023-10-16 17:35:22 +0200

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './article/article.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticleComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
